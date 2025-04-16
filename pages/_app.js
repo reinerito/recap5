5f4022eff701fca592aa46073995becca79df852
reinerito <reiner.stieger@gmail.com>
2025-04-16 10:14:32 +0200

    favoriteiconadd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
 import GlobalStyle from "../styles";
 import Navigation from "../components/Navigation";
+import { useState, useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [favorites, setFavorites] = useState([]);
+
+  // Load favorites from localStorage on mount
+  useEffect(() => {
+    const savedFavorites = localStorage.getItem("favorites");
+    if (savedFavorites) {
+      setFavorites(JSON.parse(savedFavorites));
+    }
+  }, []);
+
+  // Save favorites to localStorage when they change
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const toggleFavorite = (slug) => {
+    setFavorites((currentFavorites) => {
+      if (currentFavorites.includes(slug)) {
+        return currentFavorites.filter((favorite) => favorite !== slug);
+      } else {
+        return [...currentFavorites, slug];
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -13,7 +39,11 @@ export default function App({ Component, pageProps }) {
       }}
     >
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        favorites={favorites}
+        onToggleFavorite={toggleFavorite}
+      />
       <Navigation />
     </div>
   );
